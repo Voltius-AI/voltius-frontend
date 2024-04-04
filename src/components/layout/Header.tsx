ffc326368aafc54a1ae44f5e59bdf5a226426762
omsant02 <omsantoshwar02@gmail.com>
2024-04-04 23:03:30 +0530

    header scroll bg blur

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,10 +1,30 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      setIsScrolled(scrollPosition > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="grid grid-cols-12 gap-2 sm:grid-cols-12 border-2 justify-center items-center border-rose-500 mt-10 rounded-xl pt-4 pb-4 ">
+    <header
+      className={`fixed top-5 left-0 right-0 ml-5 mr-5 sm:ml-16 sm:mr-16 z-50 grid grid-cols-12 gap-2 sm:grid-cols-12 border-2 justify-center items-center border-rose-500 mt-10 rounded-xl pt-4 pb-4  transition-all duration-300 ${
+        isScrolled ? "backdrop-blur-md" : ""
+      }`}
+    >
       <a href="" className="col-span-5 sm:col-span-6 sm:ml-4 text-2xl ml-1">
         <Image src={"/Voltius.png"} alt="" width={100} height={100} />
       </a>
