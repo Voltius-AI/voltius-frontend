ce68c99e4419a87b4ea3ce4cd7d2fb20b9dedf36
Ayush Kumar Singh <ayushks11560@gmail.com>
2024-04-30 12:48:11 +0530

    Add NEXT_PUBLIC_API_URL environment variable

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["s8gf90.p3cdn1.secureserver.net", "cdn-images-1.medium.com"],
   },
+  env: {
+    NEXT_PUBLIC_API_URL: "http://localhost:4000/api",
+  },
 };
 
 export default nextConfig;
--- src/services/service.js
+++ src/services/service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const APIInstance = axios.create({
-  baseURL: `http://localhost:4000/api/`,
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
